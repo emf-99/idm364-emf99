f220f5e9ff5efc8fe6cd79caa8fe5f28caf566c6
emf-99 <113733097+emf-99@users.noreply.github.com>
2025-03-14 14:33:59 -0400

    :lipstick: fix header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -46,7 +46,7 @@
       background-position: top;
       width: 100%;
       height: 100px; 
-      border-radius: 0 0 60px 60px;
+      border-radius: 0 0 50px 50px;
       box-shadow: 0px 4px 54.4px 0px rgba(0, 0, 0, 0.25);
       overflow: hidden; 
       margin-bottom: -40px; 
